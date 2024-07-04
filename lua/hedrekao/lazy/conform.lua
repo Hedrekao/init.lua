78b2bc69c0ad209cb67ad1627e8b68120851e114
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-07-04 23:09:47 +0200

    formatter setup

---
+++ lua/hedrekao/lazy/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "n",
+			desc = "Format buffer",
+		},
+	},
+}
