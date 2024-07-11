cb1f131f7f719a73b6aa288cfdbcd5d565329167
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-07-12 00:20:36 +0200

    trouble setup

--- lua/hedrekao/lazy/trouble.lua
+++ lua/hedrekao/lazy/trouble.lua
@@ -1,20 +1,43 @@
 return {
-    {
-        "folke/trouble.nvim",
-        opts = {
-            auto_jump = true
-        }, -- for default options, refer to the configuration section for custom setup.
-        cmd = "Trouble",
-        keys = {
-            {
-                "<leader>tt",
-                "<cmd>Trouble diagnostics toggle<cr>",
-                desc = "Diagnostics (Trouble)",
-            },
-            {
-                "[t",
-                "next"
-            }
-        },
-    },
+	{
+		"folke/trouble.nvim",
+		opts = { auto_close = true }, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>tt",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"t]",
+				"<cmd>Trouble next jump=true<cr>",
+				desc = "Next Trouble",
+			},
+			{
+				"t[",
+				"<cmd>Trouble prev jump=true<cr>",
+				desc = "Previous Trouble",
+			},
+		},
+		icons = {
+			indent = {
+				middle = " ",
+				last = " ",
+				top = " ",
+				ws = "│  ",
+			},
+		},
+		modes = {
+			test = {
+				mode = "diagnostics",
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.3,
+				},
+			},
+		},
+	},
 }
