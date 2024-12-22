9e417f85bb1940883c52abe0a855f4ffcc92959b
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-12-22 13:35:55 +0100

    use oil nvim for the file tree

--- lua/hedrekao/remap.lua
+++ lua/hedrekao/remap.lua
@@ -37,11 +37,13 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+  "n",
+  "<leader>ee",
+  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
+
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
